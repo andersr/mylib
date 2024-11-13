5727b236ef33b1b89b02b31dfd129c9b5b55f1ea
Johan 'Anders' Ramsay <819213+andersr@users.noreply.github.com>
2024-11-13 12:50:18 -0500

    fix: Rm styled Icon story

--- apps/docs/stories/Icon.stories.ts
+++ apps/docs/stories/Icon.stories.ts
@@ -4,25 +4,14 @@ import { Icon } from "@andersco/ui";
 
 const meta = {
   component: Icon,
-  parameters: {
-    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
-  },
-  // More on argTypes: https://storybook.js.org/docs/api/argtypes
+  parameters: {},
 } satisfies Meta<typeof Icon>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const Primary: Story = {
   args: {
     name: "search",
-  },
-};
-
-export const Styled: Story = {
-  args: {
-    name: "delete_forever",
-    twStyle: "text-orange-500 text-8xl",
   },
 };
