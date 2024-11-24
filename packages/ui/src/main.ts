598a9b524f5543bc921c309464e2a6f6dd5a6c76
Johan 'Anders' Ramsay <819213+andersr@users.noreply.github.com>
2024-11-24 13:51:29 -0500

    feat(ui): :sparkles: Add IconButton component

--- packages/ui/src/main.ts
+++ packages/ui/src/main.ts
@@ -3,3 +3,4 @@ import "./tailwind.css";
 export { Button } from "./components/Button/Button";
 export { Icon } from "./components/Icon/Icon";
 export { Required } from "./components/Required/Required";
+export { IconButton } from "./components/IconButton/IconButton";
